5d106c36ffe90a7796b7d14d125f2f30b456dcf0
Ajose Oyedepo <oyedepo304@gmail.com>
2023-12-13 16:34:28 +0100

    Update AuthController.js

--- Model/Auth/AuthController.js
+++ Model/Auth/AuthController.js
@@ -44,6 +44,7 @@ exports.SignUp = catchAsync(async (req, res, next) => {
 });
 exports.Login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
+  console.log({email,password})
   if (!email || !password) {
     return next(new AppError('please provide your email or password', 400));
   }
